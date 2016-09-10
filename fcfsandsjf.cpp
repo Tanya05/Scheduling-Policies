cea4c61719daaa746e643d01c15c63a84857c15f
Prat-123 <Prat-123@users.noreply.github.com>
2016-09-10 19:29:52 +0530

    Update fcfsandsjf.cpp

--- fcfsandsjf.cpp
+++ fcfsandsjf.cpp
@@ -1,125 +1,125 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include "process.cpp"
 using namespace std;
 
-string pID[100];
-int arrivalTime[100];
-int serviceTime[100];
-int priority[100];
-int waitTime[100];
-int finishTime[100];
-int TAT[100];
-int size;
+vector <Process*> processes;
 
 void readFile() {
 	ifstream inputFile;
 	inputFile.open("config.txt");
-	int i = 0;
+	string pID;
+	int arrivalTime;
+	int serviceTime;
+	int priority;
 	while( inputFile.good() ) {
-		inputFile >> pID[i];
-		inputFile >> arrivalTime[i];
-		inputFile >> serviceTime[i];
-		inputFile >> priority[i];
-		i++;
+		
+		inputFile >> pID;
+		inputFile >> arrivalTime;
+		inputFile >> serviceTime;
+		inputFile >> priority;
+		Process* p = new Process( pID, arrivalTime, serviceTime, priority );
+		processes.push_back( p );
+		
 	}
-	size = i - 1;
 	inputFile.close();
+	
 }
 
-void SJF() {
-	
-	//this method assumes that the process appear in increasing order of arrival time
-	vector<int> q(size);
-	for( int k = 1; k < size; k++ ) {
-		if( arrivalTime[k] < finishTime[k-1] ) {
-			q.push_back(serviceTime[k]);
-		}
-		int min = 10000;
-		int ind = -1;
-		for( int i = 0; i < k; i++ ) {
-			if( q[i] < min ) {
-				min = q[i];
-				ind = i;
-			}
-		}
-
-void FCFS() {
-	//sorting arrival time using bubble sort
-	for( int i = 0; i < size; i++ ) {
-		for( int j = i+1; j < size-1; j++ ) {
-			if( arrivalTime[j] > arrivalTime[j+1] ) {
-				//whichever process arrived first
-				int t = arrivalTime[j];
-				arrivalTime[j] = arrivalTime[j+1];
-				arrivalTime[j+1] = t;
-				
-				//swapping all necessary information
-				t = priority[j];
-				priority[j] = priority[j+1];
-				priority[j+1] = t;
-				
-				t = serviceTime[j];
-				serviceTime[j] = serviceTime[j+1];
-				serviceTime[j+1] = t;
-				
-				string pid = pID[j];
-				pID[j] = pID[j+1];
-				pID[j+1] = pid;
-			}
-			else if( arrivalTime[j] == arrivalTime[j+1] ) {
-				//in case 2 processes have the same arrival time, they are scheduled according to their priority
-				if( priority[j] < priority[j+1] ) {
-					int t = arrivalTime[j];
-				arrivalTime[j] = arrivalTime[j+1];
-				arrivalTime[j+1] = t;
-				
-				t = priority[j];
-				priority[j] = priority[j+1];
-				priority[j+1] = t;
-				
-				t = serviceTime[j];
-				serviceTime[j] = serviceTime[j+1];
-				serviceTime[j+1] = t;
-				
-				string pid = pID[j];
-				pID[j] = pID[j+1];
-				pID[j+1] = pid;
-				}
-			}
-		}
+void writeFile( char* file ) {
+	ofstream outFile;
+	outFile.open(file);
+	outFile << "Process ID 	Arrival Time 	Service Time 	Priority 	Finish Time 	Turn around Time    Total Wait Time" << endl;
+	for( int i = 0; i < processes.size()-1;i++ ) {
+		//cout << i << " ";
+		outFile << processes[i]->pID << " 		" << processes[i]->arrivalTime << " 		" << processes[i]->serviceTime << " 		" << processes[i]->priority << " 		" << processes[i]->finishTime << " 		" << processes[i]->TAT << "		 	" << processes[i]->waitTime << endl;
 	}
+	outFile.close();
 }
 
 void assign() {
 	
 	//setting finish times and wait times;
 	//it is assumed that the first process arrives at time 0 and not some other instant
-	finishTime[0] = serviceTime[0];
-	waitTime[0] = 0;
-	TAT[0] = serviceTime[0];
-	for( int i = 1; i < size; i++ ) {
-		finishTime[i] = finishTime[i-1] + serviceTime[i];
-		waitTime[i] = finishTime[i-1] - arrivalTime[i];
-		if( waitTime[i] < 0 )
-			waitTime[i] = 0;
-		TAT[i] = finishTime[i] - arrivalTime[i];
+	processes[0]->finishTime = processes[0]->serviceTime;
+	processes[0]->waitTime = 0;
+	processes[0]->TAT = processes[0]->serviceTime;
+
+	for( int i = 1; i < processes.size()-1; i++ ) {
+		
+		processes[i]->finishTime = processes[i-1]->finishTime + processes[i]->serviceTime;
+		processes[i]->waitTime = processes[i-1]->finishTime - processes[i]->arrivalTime;
+		if( processes[i]->waitTime < 0 )
+			processes[i]->waitTime = 0;
+		processes[i]->TAT = processes[i]->finishTime - processes[i]->arrivalTime;
 	}
 }
 
-void writeFile() {
-	ofstream outFile;
-	outFile.open("Output.txt");
-	outFile << "Process ID 	Arrival Time 	Service Time 	Priority 	Finish Time 	Turn around Time    Total Wait Time" << endl;
-	for( int i = 0; i < size; i++ ) {
-		cout << i << " ";
-		outFile << pID[i] << " 		" << arrivalTime[i] << " 		" << serviceTime[i] << " 		" << priority[i] << " 		" << finishTime[i] << " 		" << TAT[i] << "		 	" << waitTime[i] << endl;
+class SJFCompare {
+	
+	public:
+		bool operator()( Process* p1, Process* p2 ) {
+	
+			if( p1->serviceTime < p2->serviceTime )	
+				return true;
+			else
+				return false;
+		}
+};
+
+void SJF() {
+	
+	//this method assumes that the process appear in increasing order of arrival time
+	priority_queue <Process*, vector<Process*>, SJFCompare> queue;
+	vector <Process*> sjf;
+	int currentTime = 0;
+	int i;
+	for( i = 0; i < processes.size(); i++ ) {
+		if( processes[i]->arrivalTime <= currentTime ) {
+			queue.push( processes[i] );
+		}
+		Process* current = queue.top();
+		queue.pop();
+		currentTime += current->serviceTime;
+		sjf.push_back( current );
 	}
+	
+	for( int i = 0; i < sjf.size() - 1; i++ ) {
+			processes[i] = sjf[i];
+	}
+	assign();
+	writeFile( (char*)("SJFOutput.txt") );
+		
+}
+
+bool FCFSCompare( Process* p1, Process* p2 ) {
+	
+	if( p1->arrivalTime < p2->arrivalTime )
+		return true;
+	if( p1->arrivalTime == p2->arrivalTime ) {
+		if( p1->priority < p2->priority )
+			return true;
+		else
+			return false;
+	}
+	return false;
+}
+		
+void FCFS() {
+	
+	sort( processes.begin(), processes.end(), FCFSCompare );
+	assign();
+	writeFile( (char*)("FCFSOutput.txt") );
 }
 
 int main() {
+
 	readFile();
-	FCFS();
-	assign();
-	writeFile();
+	//FCFS();
+	SJF();
 	return 0;
 }
